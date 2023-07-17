342654001d8f8bf1acb7f348d33f0fac6f1f8319
Gustavo Melo <gustamvieira@gmail.com>
2023-07-16 21:00:56 -0300

    estilização de popup do formulario

--- form.js
+++ form.js
@@ -1,13 +1,26 @@
-    // Função para mostrar o pop-up
-    function mostrarPopup(event) {
-        event.preventDefault(); // Impede o envio do formulário
-  
-        var popup = document.getElementById("popup");
-        popup.style.display = "block";
-      }
-  
-      // Função para fechar o pop-up
-      function fecharPopup() {
-        var popup = document.getElementById("popup");
-        popup.style.display = "none";
-      }
+function validarForm() {
+    // Lógica de validação do formulário aqui
+    // ...
+
+    // Exibe o pop-up se o formulário for válido
+    mostrarPopup(event);
+    return false; // Impede o envio do formulário
+}
+
+function mostrarPopup(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+function fecharPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
+
+document.addEventListener("click", function(event) {
+  var popup = document.getElementById("popup");
+  if (event.target !== popup && !popup.contains(event.target)) {
+      fecharPopup();
+  }
+});
